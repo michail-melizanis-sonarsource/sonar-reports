294cd190ecfecae34efbf0edca8d15c5dbcfccb5
Perry Stallings <perry.stallings@sonarsource.com>
2024-11-13 10:40:39 -0600

    Fixing permission template creation with project key pattern

--- src/operations/join_string.py
+++ src/operations/join_string.py
@@ -1,2 +1,7 @@
-def execute(strings, delimiter, **kwargs):
+def execute(strings, delimiter, skip_empty_string=False, **kwargs):
+    if any([s is None for s in strings]):
+        return None
+    if skip_empty_string and any([not s for s in strings]):
+        return None
     return delimiter.join(strings)
+
